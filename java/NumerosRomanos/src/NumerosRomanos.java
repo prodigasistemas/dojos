749fa4fe2034398f336acf33803675be3f9065a2
Felipe <felipe.santos@prodigasistemas.com.br>
2013-10-09 17:58:50 -0300

    Etapa 2 do projeto

--- java/NumerosRomanos/src/NumerosRomanos.java
+++ java/NumerosRomanos/src/NumerosRomanos.java
@@ -1,18 +1,71 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 
 public class NumerosRomanos {
 	
 	String numeroRomano = "";
+	int resto = 0;
 	
+	HashMap<Integer, String> mapGrupoNumeros = new LinkedHashMap<Integer, String>();
+	
+	public NumerosRomanos() {
+		mapGrupoNumeros.put(1000, "M");
+		mapGrupoNumeros.put(900, "CM");
+		mapGrupoNumeros.put(500, "D");
+		mapGrupoNumeros.put(400, "CD");
+		mapGrupoNumeros.put(100, "C");
+		mapGrupoNumeros.put(90, "XC");
+		mapGrupoNumeros.put(50, "L");
+		mapGrupoNumeros.put(40, "XL");
+		mapGrupoNumeros.put(10, "X");
+		mapGrupoNumeros.put(9, "IX");
+		mapGrupoNumeros.put(5, "V");
+		mapGrupoNumeros.put(4, "IV");
+		mapGrupoNumeros.put(1, "I");
+		
+	}
 	public String converte(int numero) {
 		
-		int resto = numero % 100;
-		if ( numero >= 100){
-			concatena(numero, 100, 'C');
+		resto = numero % 1000;
+		
+		if ( numero >= 1000){
+			concatena(numero, 1000, 'M');
 		}
 
+		if(resto >= 900){
+			numeroRomano += "CM";
+			resto = resto - 900;
+		}
+		
+		if(resto >= 500){
+			numeroRomano += "D";
+			resto = resto - 500;
+		}
+		
+		if (resto >= 400){
+			numeroRomano += "CD";
+			resto = resto - 400;
+		}
+		
+		if ( resto >= 100){
+			concatena(resto, 100, 'C');
+			resto = resto % 100;
+		}
+
+		if(resto >= 90){
+			numeroRomano += "XC";
+			resto = resto - 90;
+		}
+		
 		if(resto >= 50){
 			numeroRomano += "L";
 			resto = resto - 50;
+		}
+		
+		if (resto >= 40){
+			numeroRomano += "XL";
+			resto = resto - 40;
 		}
 
 		if(resto >= 10){
@@ -20,6 +73,11 @@ public class NumerosRomanos {
 			resto = resto % 10;
 		}
 		
+		if(resto >= 9){
+			numeroRomano += "IX";
+			resto = resto - 9;
+		}
+
 		if(resto >= 5){
 			numeroRomano += "V";
 			resto = resto - 5;
@@ -32,6 +90,14 @@ public class NumerosRomanos {
 			concatena(resto, 1, 'I');
 		}
 		
+		return numeroRomano;
+	}
+	
+	private String retornaNumeroRomano(int numero) {
+		
+		for (Integer chave : iterable) {
+			
+		}
 		
 		return numeroRomano;
 	}
--- java/NumerosRomanos/test/NumerosRomanosTest.java
+++ java/NumerosRomanos/test/NumerosRomanosTest.java
@@ -22,4 +22,35 @@ public class NumerosRomanosTest extends TestCase {
 		NumerosRomanos numeros = new NumerosRomanos();
 		assertEquals("CLVIII", numeros.converte(158));
 	}
+	
+	public void teste37() {
+		NumerosRomanos numeros = new NumerosRomanos();
+		assertEquals("XXXVII", numeros.converte(37));
+	}
+	
+	public void teste96() {
+		NumerosRomanos numeros = new NumerosRomanos();
+		assertEquals("XCVI", numeros.converte(96));
+	}
+	
+	public void teste149() {
+		NumerosRomanos numeros = new NumerosRomanos();
+		assertEquals("CXLIX", numeros.converte(149));
+	}
+
+	public void teste499() {
+		NumerosRomanos numeros = new NumerosRomanos();
+		assertEquals("CDXCIX", numeros.converte(499));
+	}	
+
+	public void teste1173() {
+		NumerosRomanos numeros = new NumerosRomanos();
+		assertEquals("MCLXXIII", numeros.converte(1173));
+	}	
+
+	public void teste2344() {
+		NumerosRomanos numeros = new NumerosRomanos();
+		assertEquals("MMCCCXLIV", numeros.converte(2344));
+	}	
+
 }
