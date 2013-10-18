cc74fb90d3947ed4f94f50a89c6c1531d4a598fc
Felipe <felipe.santos@prodigasistemas.com.br>
2013-10-18 09:28:05 -0300

    Adicionando alterações do projeto NumerosRomanos

--- java/NumerosRomanos/src/NumerosRomanos.java
+++ java/NumerosRomanos/src/NumerosRomanos.java
@@ -7,106 +7,80 @@ public class NumerosRomanos {
 	String numeroRomano = "";
 	int resto = 0;
 	
-	HashMap<Integer, String> mapGrupoNumeros = new LinkedHashMap<Integer, String>();
+	HashMap<Integer, CallBack> mapGrupoNumeros = new LinkedHashMap<Integer, CallBack>();
 	
 	public NumerosRomanos() {
-		mapGrupoNumeros.put(1000, "M");
-		mapGrupoNumeros.put(900, "CM");
-		mapGrupoNumeros.put(500, "D");
-		mapGrupoNumeros.put(400, "CD");
-		mapGrupoNumeros.put(100, "C");
-		mapGrupoNumeros.put(90, "XC");
-		mapGrupoNumeros.put(50, "L");
-		mapGrupoNumeros.put(40, "XL");
-		mapGrupoNumeros.put(10, "X");
-		mapGrupoNumeros.put(9, "IX");
-		mapGrupoNumeros.put(5, "V");
-		mapGrupoNumeros.put(4, "IV");
-		mapGrupoNumeros.put(1, "I");
+		mapGrupoNumeros.put(1000, new Concatena("M"));
+		mapGrupoNumeros.put(900, new Append("CM"));
+		mapGrupoNumeros.put(500, new Append("D"));
+		mapGrupoNumeros.put(400, new Append("CD"));
+		mapGrupoNumeros.put(100, new Concatena("C"));
+		mapGrupoNumeros.put(90, new Append("XC"));
+		mapGrupoNumeros.put(50, new Append("L"));
+		mapGrupoNumeros.put(40, new Append("XL"));
+		mapGrupoNumeros.put(10, new Concatena("X"));
+		mapGrupoNumeros.put(9, new Append("IX"));
+		mapGrupoNumeros.put(5, new Append("V"));
+		mapGrupoNumeros.put(4, new Append("IV"));
+		mapGrupoNumeros.put(1, new Concatena("I"));
 		
 	}
+	
 	public String converte(int numero) {
 		
-		resto = numero % 1000;
-		
-		if ( numero >= 1000){
-			concatena(numero, 1000, 'M');
-		}
+		resto = numero ;
 
-		if(resto >= 900){
-			numeroRomano += "CM";
-			resto = resto - 900;
-		}
-		
-		if(resto >= 500){
-			numeroRomano += "D";
-			resto = resto - 500;
-		}
-		
-		if (resto >= 400){
-			numeroRomano += "CD";
-			resto = resto - 400;
-		}
-		
-		if ( resto >= 100){
-			concatena(resto, 100, 'C');
-			resto = resto % 100;
-		}
-
-		if(resto >= 90){
-			numeroRomano += "XC";
-			resto = resto - 90;
-		}
-		
-		if(resto >= 50){
-			numeroRomano += "L";
-			resto = resto - 50;
-		}
-		
-		if (resto >= 40){
-			numeroRomano += "XL";
-			resto = resto - 40;
-		}
-
-		if(resto >= 10){
-			concatena(resto, 10, 'X');
-			resto = resto % 10;
-		}
-		
-		if(resto >= 9){
-			numeroRomano += "IX";
-			resto = resto - 9;
-		}
-
-		if(resto >= 5){
-			numeroRomano += "V";
-			resto = resto - 5;
-		}
-		
-		if (resto == 4) {
-			numeroRomano +=  "IV";
-		}
-		else {
-			concatena(resto, 1, 'I');
+		for (Integer chave : mapGrupoNumeros.keySet()) {
+			if(resto >= chave)
+				mapGrupoNumeros.get(chave).executa(resto, chave);
 		}
 		
 		return numeroRomano;
 	}
+
 	
-	private String retornaNumeroRomano(int numero) {
+	abstract class CallBack{
 		
-		for (Integer chave : iterable) {
+		protected String algarismo;
+
+		public CallBack(String algarismo) {
+			this.algarismo = algarismo;
+		}
+		
+		abstract void executa(int numero, int divisor);
+		
+	}
+	
+	class Concatena extends CallBack{
+
+		public Concatena(String algarismo) {
+			super(algarismo);
+		}
+
+		public void executa(int numero, int divisor) {
+
+			int mod = numero / divisor;
+			for(int i = 0; i < mod; i++){
+				numeroRomano += algarismo;
+			}
+			resto = resto % divisor;
+		}
+		
+	}
+	
+	class Append extends CallBack{
+
+		public Append(String algarismo) {
+			super(algarismo);
+		}
+
+		public void executa(int numero, int divisor) {
+
+			numeroRomano += algarismo;
+			resto = resto - divisor;
 			
 		}
 		
-		return numeroRomano;
-	}
-
-	private void concatena(int numero, int divisor, char algarismo) {
-		int mod = numero / divisor;
-		for(int i = 0; i < mod; i++){
-			numeroRomano += algarismo;
-		}
 	}
 
 }
